7975eccef2131e65c11c763a4140b407fdfd6a38
samarth-sheth-us <samarth.sheth@logicwind.com>
2025-05-08 12:02:32 +0530

    v4

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ const App = () => {
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>CODEPUSH v3</Text>
+      <Text>CODEPUSH v4</Text>
       {codepushStatus && <Text>Status: {codepushStatus}</Text>}
     </View>
   );
